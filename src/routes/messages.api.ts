44cc15056bb6e0229ffd19a7714e6902749d5cc3
chrisoney <christopher.oney@yahoo.com>
2021-03-03 20:59:22 -0800

    attempt to separate out cron messaging loop and switch to console.error

--- src/routes/messages.api.ts
+++ src/routes/messages.api.ts
@@ -1,136 +1,127 @@
 /* eslint-disable no-shadow */
 import express from 'express';
+var cron = require('node-cron');
+const ObjectsToCsv = require('objects-to-csv');
 import { ObjectId } from 'mongodb';
 import auth from '../middleware/auth';
-import { Message } from '../models/message.model';
-import { MessageTemplate } from '../models/messageTemplate.model';
-import { Outcome } from '../models/outcome.model';
-import { Patient } from '../models/patient.model';
+import { Message, IMessage } from '../models/message.model';
+import { MessageTemplate, IMesssageTemplate } from '../models/messageTemplate.model';
+import { Outcome, IOutcome } from '../models/outcome.model';
+import { Patient, IPatient } from '../models/patient.model';
+
 
 import initializeScheduler from '../utils/scheduling';
 import errorHandler from './error';
 
-const cron = require('node-cron');
-
 const router = express.Router();
 initializeScheduler();
 
-// run messages every day at midnight PST
-cron.schedule(
-  '0 */5 * * * *',
-  () => {
-    console.log('Running batch of schdueled messages');
-    Patient.find().then((patients) => {
-      MessageTemplate.find({ type: 'Initial' })
-        .then((MessageTemplates) => {
-          patients.forEach((patient) => {
-            if (patient.enabled) {
-              const messages = MessageTemplates.filter(
-                (template) =>
-                  template.language.toLowerCase() ===
-                  patient.language.toLowerCase(),
-              );
-              if (messages.length < 1) {
-                console.log(
-                  'Unable to find message appropriate for member = ',
-                  patient._id,
-                );
-                return;
-              }
-              const randomVal = Math.floor(Math.random() * messages.length);
-              const message = messages[randomVal].text;
-              const date = new Date();
-              date.setMinutes(date.getMinutes() + 1);
-              const newMessage = new Message({
-                patientID: new ObjectId(patient._id),
-                phoneNumber: patient.phoneNumber,
-                date,
-                message,
-                sender: 'BOT',
-                sent: false,
-              });
-              newMessage.save();
-            }
-          });
-        })
-        .catch((err) => console.log(err));
+const cycleThroughPatients = (MessageTemplates: IMesssageTemplate[], patients: IPatient[]) => {
+  for (const patient of patients) {
+    if(patient.enabled) {
+      const messages = MessageTemplates.filter(template => template.language === patient.language);
+      const randomVal =  Math.floor(Math.random() * (messages.length));
+      const message = messages[randomVal].text;
+      addNewMessageForPatient(patient, message);
+    }
+  }
+}
+
+const addNewMessageForPatient = (patient: IPatient, message: string) => {
+  try {
+    var date = new Date();
+    date.setMinutes(date.getMinutes() + 1);
+    const newMessage = new Message({
+      patientID: new ObjectId(patient._id),
+      phoneNumber: patient.phoneNumber,
+      date: date,
+      message: message,
+      sender: 'BOT',
+      sent: false
     });
-  },
-  {
+    newMessage.save();
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+//run messages every day at midnight PST
+cron.schedule('0 0 0 * * *', () => {
+  console.log("Running batch of schdueled messages");
+  Patient.find().then((patients: IPatient[]) => {
+    MessageTemplate.find({type: "Initial"}).then((MessageTemplates: IMesssageTemplate[]) => {
+      cycleThroughPatients(MessageTemplates, patients);
+    })
+  });
+  },{
     scheduled: true,
-    timezone: 'America/Los_Angeles',
-  },
-);
+    timezone: "America/Los_Angeles"
+});
+
+
 
 router.post('/newMessage', auth, async (req, res) => {
   // validate phone number
-  if (
-    !req.body.phoneNumber ||
-    req.body.phoneNumber.match(/\d/g) === null ||
-    req.body.phoneNumber.match(/\d/g).length !== 10
-  ) {
+  if (!req.body.phoneNumber || req.body.phoneNumber.match(/\d/g) == null ||  req.body.phoneNumber.match(/\d/g).length !== 10){
     return res.status(400).json({
-      msg: 'Unable to add message: invalid phone number',
+      msg: 'Unable to add message: invalid phone number'
+    }); 
+  }
+
+  if (!req.body.patientID || req.body.patientID == ''){
+    return res.status(400).json({
+      msg: 'Unable to add message: must include patient ID'
     });
   }
 
-  if (!req.body.patientID || req.body.patientID === '') {
+  if (!req.body.sender || req.body.sender == ''){
     return res.status(400).json({
-      msg: 'Unable to add message: must include patient ID',
+      msg: 'Unable to add message: must include sender'
     });
   }
 
-  if (!req.body.sender || req.body.sender === '') {
+  if (!req.body.date || req.body.date == ''){
     return res.status(400).json({
-      msg: 'Unable to add message: must include sender',
+      msg: 'Unable to add message: must include date'
     });
   }
 
-  if (!req.body.date || req.body.date === '') {
-    return res.status(400).json({
-      msg: 'Unable to add message: must include date',
-    });
-  }
-
-  if (req.body.image === null) {
+  if (req.body.image == null) {
     const newMessage = new Message({
       phoneNumber: req.body.phoneNumber,
       patientID: req.body.patientID,
       message: req.body.message,
       sender: req.body.sender,
-      date: req.body.date,
+      date: req.body.date
     });
-    return newMessage.save().then(() => {
-      return res.status(200).json({
-        success: true,
+    return newMessage.save().then( () => {
+      
+      res.status(200).json({
+        success: true
       });
     });
-  }
-
-  throw new Error('something went wrong, I should not have gotten here');
+  } 
+    
 });
+
 
 router.post('/newOutcome', auth, async (req, res) => {
   // validate phone number
-  if (
-    !req.body.phoneNumber ||
-    req.body.phoneNumber.match(/\d/g) === null ||
-    req.body.phoneNumber.match(/\d/g).length !== 10
-  ) {
+  if (!req.body.phoneNumber || req.body.phoneNumber.match(/\d/g) == null ||  req.body.phoneNumber.match(/\d/g).length !== 10){
     return res.status(400).json({
-      msg: 'Unable to add outcome: invalid phone number',
+      msg: 'Unable to add outcome: invalid phone number'
     });
   }
 
-  if (req.body.patientID === '') {
+  if (req.body.patientID == ''){
     return res.status(400).json({
-      msg: 'Unable to add outcome: must include patient ID',
+      msg: 'Unable to add outcome: must include patient ID'
     });
   }
 
-  if (req.body.language === '') {
+  if (req.body.language == ''){
     return res.status(400).json({
-      msg: 'Unable to add outcome: must include language',
+      msg: 'Unable to add outcome: must include language'
     });
   }
 
@@ -140,40 +131,33 @@ router.post('/newOutcome', auth, async (req, res) => {
     date: req.body.date,
     response: req.body.response,
     value: req.body.value,
-    alertType: req.body.alertType,
+    alertType: req.body.alertType
   });
-  Patient.findOneAndUpdate(
-    { _id: req.body.patientID },
-    { $inc: { responseCount: 1 } },
-  );
-  return newOutcome.save().then(() => {
+  Patient.findOneAndUpdate({_id : req.body.patientID}, {$inc: {responseCount: 1}});
+  return newOutcome.save().then( () => {
     res.status(200).json({
-      success: true,
+      success: true
     });
   });
 });
 
 router.post('/scheduledMessage', auth, async (req, res) => {
   // validate phone number
-  if (
-    !req.body.phoneNumber ||
-    !req.body.phoneNumber.match(/\d/g) ||
-    req.body.phoneNumber.match(/\d/g).length !== 10
-  ) {
+  if (!req.body.phoneNumber || req.body.phoneNumber.match(/\d/g) == null ||  req.body.phoneNumber.match(/\d/g).length !== 10){
     return res.status(400).json({
-      msg: 'Unable to add outcome: invalid phone number',
+      msg: 'Unable to add outcome: invalid phone number'
     });
   }
 
-  if (req.body.patientID === '') {
+  if (req.body.patientID == ''){
     return res.status(400).json({
-      msg: 'Unable to add outcome: must include patient ID',
+      msg: 'Unable to add outcome: must include patient ID'
     });
   }
 
-  if (req.body.language === '') {
+  if (req.body.language == ''){
     return res.status(400).json({
-      msg: 'Unable to add outcome: must include language',
+      msg: 'Unable to add outcome: must include language'
     });
   }
 
@@ -183,25 +167,24 @@ router.post('/scheduledMessage', auth, async (req, res) => {
     date: req.body.date,
     response: req.body.response,
     value: req.body.value,
-    alertType: req.body.alertType,
+    alertType: req.body.alertType
   });
-  return newMessage.save().then(() => {
-    Patient.findByIdAndUpdate(new ObjectId(req.body.patientId), {
-      $inc: { messagesSent: 1 },
-    });
+  return newMessage.save().then( () => {
+    Patient.findByIdAndUpdate(new ObjectId(req.body.patientId), { $inc: { messagesSent : 1}});
     res.status(200).json({
-      success: true,
+      success: true
     });
   });
 });
 
 router.get('/allOutcomes', auth, async (req, res) => {
   return Outcome.find()
-    .then((outcomesList) => {
-      Patient.find().then((patientList) => {
-        res.status(200).send({ outcomes: outcomesList, patients: patientList });
-      });
-    })
-    .catch((err) => errorHandler(res, err.msg));
+  .then((outcomesList) => {
+    Patient.find().then((patientList) => {
+      res.status(200).send({outcomes: outcomesList, patients: patientList});
+    });
+
+  })
+  .catch((err) => errorHandler(res, err.msg))
 });
 export default router;
