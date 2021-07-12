c57ddbdba8cdc9f7f370170fe9a5cec18979783b
cesar-vega-unosquare <cesarv@coachmehealth.org>
2021-07-12 18:10:12 -0500

    remove fallback logic, fix linter

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,16 +5,9 @@ export const JWT_SECRET = process.env.JWT_SECRET || '';
 export const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || '';
 export const SENDGRID_EMAIL = 'hello@email.com';
 
-export const parseTwilioFromNumber = (
-  rawNumber: string | undefined,
-  backupNumber?: string | undefined,
-) => {
+export const parseTwilioFromNumber = (rawNumber: string | undefined) => {
   if (!rawNumber) {
-    if (!backupNumber) {
-      throw new Error('No TWILIO_FROM_NUMBER Found can not run server');
-    } else {
-      return backupNumber.replace(/[^0-9.]/g, '');
-    }
+    throw new Error('No TWILIO_FROM_NUMBER Found can not run server');
   }
   return rawNumber.replace(/[^0-9.]/g, '');
 };
@@ -27,5 +20,4 @@ export const TWILIO_FROM_NUMBER = parseTwilioFromNumber(
 
 export const TWILIO_FROM_NUMBER_GENERAL = parseTwilioFromNumber(
   process.env.TWILIO_FROM_NUMBER_GENERAL,
-  process.env.TWILIO_FROM_NUMBER,
 );
